d5e278a2100095fda406a021092089f50d2056bc
Jérôme Odier <odier.jerome@gmail.com>
2017-10-25 16:04:54 +0200

    update

--- ttt.py
+++
@@ -1,7 +0,0 @@
-import sys, pyAMI.parser
-
-print('# ' + pyAMI.parser.parse(['GetUserInfo', '-amiLogin=bar', '--foo']))
-
-pyAMI.parser.shell_barrier = True
-
-print('# ' + pyAMI.parser.parse(sys.argv[1: ]))
